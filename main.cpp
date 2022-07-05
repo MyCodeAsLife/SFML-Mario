8a2eeff6771cc7628417d9431d090d80d3da3819
MyCodeAsLife <mycodeaslife@gmail.com>
2022-07-05 16:07:41 +0500

    1

--- main.cpp
+++ main.cpp
@@ -1,252 +1,72 @@
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <cmath>
 #include <ctime>
 #include <cstdlib>
 
+using namespace sf;
 
-////////////////////////////////////////////////////////////
-/// Entry point of application
-///
-/// \return Application exit code
-///
-////////////////////////////////////////////////////////////
 int main()
 {
-	std::srand(static_cast<unsigned int>(std::time(NULL)));
+	RenderWindow window(VideoMode(640, 480), "This Mario!");
+	window.setFramerateLimit(120);
 
-	// Define some constants
-	const float pi = 3.14159f;
-	const int gameWidth = 800;
-	const int gameHeight = 600;
-	sf::Vector2f paddleSize(25, 100);
-	float ballRadius = 10.f;
+	Texture hero_texture;
+	hero_texture.loadFromFile("resources/images/fang.png");
+	Sprite hero_sprite;
+	hero_sprite.setTexture(hero_texture);
+	hero_sprite.setTextureRect(IntRect(0, 244, 40, 50));
+	hero_sprite.setOrigin(20, 25);
+	hero_sprite.setPosition(70, 70);
 
-	// Create the window of the application
-	sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight, 32), "SFML Pong",
-		sf::Style::Titlebar | sf::Style::Close);
-	window.setVerticalSyncEnabled(true);
+	//bool moveToLeft(false);
+	//bool moveToRight(false);
 
-	// Load the sounds used in the game
-	sf::SoundBuffer ballSoundBuffer;
-	if (!ballSoundBuffer.loadFromFile("resources/ball.wav"))
-		return EXIT_FAILURE;
-	sf::Sound ballSound(ballSoundBuffer);
 
-	// Create the left paddle
-	sf::RectangleShape leftPaddle;
-	leftPaddle.setSize(paddleSize - sf::Vector2f(3, 3));
-	leftPaddle.setOutlineThickness(3);
-	leftPaddle.setOutlineColor(sf::Color::Black);
-	leftPaddle.setFillColor(sf::Color(100, 100, 200));
-	leftPaddle.setOrigin(paddleSize / 2.f);
-
-	// Create the right paddle
-	sf::RectangleShape rightPaddle;
-	rightPaddle.setSize(paddleSize - sf::Vector2f(3, 3));
-	rightPaddle.setOutlineThickness(3);
-	rightPaddle.setOutlineColor(sf::Color::Black);
-	rightPaddle.setFillColor(sf::Color(200, 100, 100));
-	rightPaddle.setOrigin(paddleSize / 2.f);
-
-	// Create the ball
-	sf::CircleShape ball;
-	ball.setRadius(ballRadius - 3);
-	ball.setOutlineThickness(3);
-	ball.setOutlineColor(sf::Color::Black);
-	ball.setFillColor(sf::Color::White);
-	ball.setOrigin(ballRadius / 2, ballRadius / 2);
-
-	// Load the text font
-	sf::Font font;
-	if (!font.loadFromFile("resources/sansation.ttf"))
-		return EXIT_FAILURE;
-
-	// Initialize the pause message
-	sf::Text pauseMessage;
-	pauseMessage.setFont(font);
-	pauseMessage.setCharacterSize(40);
-	pauseMessage.setPosition(170.f, 150.f);
-	pauseMessage.setFillColor(sf::Color::White);
-	pauseMessage.setString("Welcome to SFML pong!\nPress space to start the game");
-
-	// Define the paddles properties
-	sf::Clock AITimer;
-	const sf::Time AITime = sf::seconds(0.1f);
-	const float paddleSpeed = 400.f;
-	float rightPaddleSpeed = 0.f;
-	const float ballSpeed = 400.f;
-	float ballAngle = 0.f; // to be changed later
-
-	sf::Clock clock;
-	bool isPlaying = false;
 	while (window.isOpen())
 	{
-		// Handle events
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event))
 		{
-			// Window closed or escape key pressed: exit
-			if ((event.type == sf::Event::Closed) ||
-				((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)))
-			{
+			if (event.type == Event::Closed)
 				window.close();
-				break;
-			}
 
-			// Space key pressed: play
-			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space))
-			{
-				if (!isPlaying)
-				{
-					// (re)start the game
-					isPlaying = true;
-					clock.restart();
-
-					// Reset the position of the paddles and ball
-					leftPaddle.setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-					rightPaddle.setPosition(gameWidth - 10 - paddleSize.x / 2, gameHeight / 2);
-					ball.setPosition(gameWidth / 2, gameHeight / 2);
-
-					// Reset the ball angle
-					do
-					{
-						// Make sure the ball initial angle is not too much vertical
-						ballAngle = (std::rand() % 360) * 2 * pi / 360;
-					} while (std::abs(std::cos(ballAngle)) < 0.7f);
-				}
-			}
 		}
 
-		if (isPlaying)
+		if (Keyboard::isKeyPressed(Keyboard::Left))
 		{
-			float deltaTime = clock.restart().asSeconds();
-
-			// Move the player's paddle
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) &&
-				(leftPaddle.getPosition().y - paddleSize.y / 2 > 5.f))
-			{
-				leftPaddle.move(0.f, -paddleSpeed * deltaTime);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) &&
-				(leftPaddle.getPosition().y + paddleSize.y / 2 < gameHeight - 5.f))
-			{
-				leftPaddle.move(0.f, paddleSpeed * deltaTime);
-			}
-
-			// Move the computer's paddle
-			if (((rightPaddleSpeed < 0.f) && (rightPaddle.getPosition().y - paddleSize.y / 2 > 5.f)) ||
-				((rightPaddleSpeed > 0.f) && (rightPaddle.getPosition().y + paddleSize.y / 2 < gameHeight - 5.f)))
-			{
-				rightPaddle.move(0.f, rightPaddleSpeed * deltaTime);
-			}
-
-			// Update the computer's paddle direction according to the ball position
-			if (AITimer.getElapsedTime() > AITime)
-			{
-				AITimer.restart();
-				if (ball.getPosition().y + ballRadius > rightPaddle.getPosition().y + paddleSize.y / 2)
-					rightPaddleSpeed = paddleSpeed;
-				else if (ball.getPosition().y - ballRadius < rightPaddle.getPosition().y - paddleSize.y / 2)
-					rightPaddleSpeed = -paddleSpeed;
-				else
-					rightPaddleSpeed = 0.f;
-			}
-
-			// Move the ball
-			float factor = ballSpeed * deltaTime;
-			ball.move(std::cos(ballAngle) * factor, std::sin(ballAngle) * factor);
-
-			// Check collisions between the ball and the screen
-			if (ball.getPosition().x - ballRadius < 0.f)
-			{
-				isPlaying = false;
-				pauseMessage.setString("You lost!\nPress space to restart or\nescape to exit");
-			}
-			if (ball.getPosition().x + ballRadius > gameWidth)
-			{
-				isPlaying = false;
-				pauseMessage.setString("You won!\nPress space to restart or\nescape to exit");
-			}
-			if (ball.getPosition().y - ballRadius < 0.f)
-			{
-				ballSound.play();
-				ballAngle = -ballAngle;
-				ball.setPosition(ball.getPosition().x, ballRadius + 0.1f);
-			}
-			if (ball.getPosition().y + ballRadius > gameHeight)
-			{
-				ballSound.play();
-				ballAngle = -ballAngle;
-				ball.setPosition(ball.getPosition().x, gameHeight - ballRadius - 0.1f);
-			}
-
-			// Check the collisions between the ball and the paddles
-			// Left Paddle
-			if (ball.getPosition().x - ballRadius < leftPaddle.getPosition().x + paddleSize.x / 2 &&
-				ball.getPosition().x - ballRadius > leftPaddle.getPosition().x &&
-				ball.getPosition().y + ballRadius >= leftPaddle.getPosition().y - paddleSize.y / 2 &&
-				ball.getPosition().y - ballRadius <= leftPaddle.getPosition().y + paddleSize.y / 2)
-			{
-				if (ball.getPosition().y > leftPaddle.getPosition().y)
-					ballAngle = pi - ballAngle + (std::rand() % 20) * pi / 180;
-				else
-					ballAngle = pi - ballAngle - (std::rand() % 20) * pi / 180;
-
-				ballSound.play();
-				ball.setPosition(leftPaddle.getPosition().x + ballRadius + paddleSize.x / 2 + 0.1f, ball.getPosition().y);
-			}
-
-			// Right Paddle
-			if (ball.getPosition().x + ballRadius > rightPaddle.getPosition().x - paddleSize.x / 2 &&
-				ball.getPosition().x + ballRadius < rightPaddle.getPosition().x &&
-				ball.getPosition().y + ballRadius >= rightPaddle.getPosition().y - paddleSize.y / 2 &&
-				ball.getPosition().y - ballRadius <= rightPaddle.getPosition().y + paddleSize.y / 2)
-			{
-				if (ball.getPosition().y > rightPaddle.getPosition().y)
-					ballAngle = pi - ballAngle + (std::rand() % 20) * pi / 180;
-				else
-					ballAngle = pi - ballAngle - (std::rand() % 20) * pi / 180;
-
-				ballSound.play();
-				ball.setPosition(rightPaddle.getPosition().x - ballRadius - paddleSize.x / 2 - 0.1f, ball.getPosition().y);
-			}
+			hero_sprite.move(-1.3f, 0.f);
+			static float currentFrame(0);
+			currentFrame += 0.07;
+			if (currentFrame > 6)
+				currentFrame -= 6;
+			// Зеркальное отображение картинки
+			hero_sprite.setTextureRect(IntRect(40 * static_cast<int>(currentFrame)+40, 244, -40, 50));
 		}
-
-		// Clear the window
-		window.clear(sf::Color(50, 200, 50));
-
-		if (isPlaying)
+		if (Keyboard::isKeyPressed(Keyboard::Right))
 		{
-			// Draw the paddles and the ball
-			window.draw(leftPaddle);
-			window.draw(rightPaddle);
-			window.draw(ball);
+			hero_sprite.move(1.3f, 0.f);
+			static float currentFrame(0);
+			currentFrame += 0.07;
+			if (currentFrame > 6)
+				currentFrame -= 6;
+			hero_sprite.setTextureRect(IntRect(40 * static_cast<int>(currentFrame), 244, 40, 50));
 		}
-		else
+		if (Keyboard::isKeyPressed(Keyboard::Up))
 		{
-			// Draw the pause message
-			window.draw(pauseMessage);
+			hero_sprite.move(0.f, -1.3f);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+		{
+			hero_sprite.move(0.f, 1.3f);
 		}
 
-		// Display things on screen
+		// Очистка экрана
+		window.clear(Color(70, 70, 70));
+		// Отрисовка персонажа
+		window.draw(hero_sprite);
 		window.display();
 	}
-
-	return EXIT_SUCCESS;
+	
+	return 0;
 }
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
